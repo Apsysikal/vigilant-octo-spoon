a009bd5919cc1c0ee0074675cfe053777c459947
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2021-10-18 16:41:03 +0200

    Created interfaces.

---
+++ index.ts
@@ -0,0 +1,51 @@
+export interface Root {
+    canton: string
+    zip: string
+    number: number
+    buildings: Building[]
+}
+  
+export interface Building {
+    buildingNumber: number
+    buildingDescription: string
+    floors: Floor[]
+}
+  
+export interface Floor {
+    floor: string
+    cabinets: Cabinet[]
+}
+  
+export interface Cabinet {
+    cabinetDescription: string
+    plants: Plant[]
+}
+
+export interface Plant {
+    plantType: string
+    plantNumber: number
+    plantDescription: string
+    devices: Device[]
+}
+
+export interface Device {
+    deviceType: string
+    deviceNumber: number
+    description: string
+    manufacturer: string
+    model: string
+    comments: Comment[]
+    dataPoints: DataPoint[]
+}
+
+export interface Comment {
+    dateCreated: string
+    dateModified: string
+    text: string
+}
+
+export interface DataPoint {
+    dataPointType: string
+    dataPointNumber: number
+    description: string
+}
